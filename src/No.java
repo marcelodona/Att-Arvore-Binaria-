003d402b3c2b41f9eb05710ab0e568db78553c7d
marcelodona <marcelo.donaf@gmail.com>
2025-06-09 17:11:16 -0300

    Update No.java

--- src/No.java
+++ src/No.java
@@ -1,34 +1,46 @@
 public class No {
+    // atributo que guarda o valor do nó
     private int conteudo;
+
+    // aponta para o filho da esquerda
     private No esquerda;
+
+    // aponta para o filho da direita
     private No direita;
 
+    // construtor do nó
     public No(int conteudo) {
         this.conteudo = conteudo;
         this.esquerda = null;
         this.direita = null;
     }
 
+    // retorna o valor armazenado no nó
     public int getConteudo() {
         return conteudo;
     }
 
+    // altera o valor armazenado no nó
     public void setConteudo(int conteudo) {
         this.conteudo = conteudo;
     }
 
+    // rtorna o filho da esquerda
     public No getEsquerda() {
         return esquerda;
     }
 
+    // define o filho da esquerda
     public void setEsquerda(No esquerda) {
         this.esquerda = esquerda;
     }
 
+    // retorna o filho da direita
     public No getDireita() {
         return direita;
     }
 
+    // define o filho da direita
     public void setDireita(No direita) {
         this.direita = direita;
     }
